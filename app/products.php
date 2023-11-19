14d8383665c914dfc16ae6b10cdef29c1d753a0a
davimouravilaca <davimouravilaca@gmail.com>
2023-11-18 23:42:03 -0300

    create-and-read

--- app/products.php
+++ app/products.php
@@ -1,18 +1,16 @@
 <?php
 
-$product1 = array(
-    "Camisa", "Mangas longas estampada", "foto", "14,98"
-);
-
-$product2 = array(
-    "nome2", "descrição2", "foto2", "16,20"
-);
-
-$products = array($product1, $product2);
-
 require "php/functions.php";
+require "php/conn.php";
+
+$sql = "SELECT * FROM products";
+
+$products = mysqli_query($conn, $sql);
 
 foreach ($products as $product) {
     createProducts($product);
     echo "<br>";
 }
+?>
+
+<a href="form.html">Cadastrar produtos</a>  
